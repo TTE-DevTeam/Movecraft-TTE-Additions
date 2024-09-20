8c7aa2e777c36699c63ddf023561450414bb3fe2
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-09-20 21:28:58 +0200

    base implementation for reverse sign

--- src/main/java/de/dertoaster/movecrafttteadditions/sign/ReverseCruiseSign.java
+++ src/main/java/de/dertoaster/movecrafttteadditions/sign/ReverseCruiseSign.java
@@ -1,2 +1,27 @@
-package de.dertoaster.movecrafttteadditions.sign;public class ReverseCruiseSign {
+package de.dertoaster.movecrafttteadditions.sign;
+
+import net.countercraft.movecraft.CruiseDirection;
+import net.countercraft.movecraft.craft.BaseCraft;
+import net.countercraft.movecraft.craft.Craft;
+import net.countercraft.movecraft.sign.CruiseSign;
+import net.countercraft.movecraft.sign.SignListener;
+import org.bukkit.entity.Player;
+
+public class ReverseCruiseSign extends CruiseSign {
+
+    public ReverseCruiseSign(String ident) {
+        super(ident);
+    }
+
+    @Override
+    protected CruiseDirection getCruiseDirection(SignListener.SignWrapper sign) {
+        return super.getCruiseDirection(sign).getOpposite();
+    }
+
+    @Override
+    protected void setCraftCruising(Player player, CruiseDirection direction, Craft craft) {
+        super.setCraftCruising(player, direction, craft);
+        // TODO: Add speed multiplier for crafts
+        // craft.setCruiseTickRateMultiplier(1.5D)
+    }
 }
