09da52e1bbe386ed1d93bed17c891f4a701b6a1c
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-09-22 14:54:08 +0200

    register method

--- src/main/java/de/dertoaster/movecrafttteadditions/init/TTEAdditionsCraftDataTags.java
+++ src/main/java/de/dertoaster/movecrafttteadditions/init/TTEAdditionsCraftDataTags.java
@@ -10,4 +10,8 @@ public class TTEAdditionsCraftDataTags {
 
     public static final CraftDataTagKey<Location> CRAFT_SPAWNPOINT = CraftDataTagRegistry.INSTANCE.registerTagKey(new NamespacedKey(TTEAdditionsPlugin.INSTANCE, "craft_spawnpoint"), craft ->new Location(craft.getWorld(), craft.getHitBox().getMidPoint().getX(), craft.getHitBox().getMidPoint().getY(), craft.getHitBox().getMidPoint().getZ()));
 
+    public static void register() {
+        // Does not need to do anything
+    }
+
 }
