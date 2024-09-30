456afe1334d48fa312f0bcbe10ae0c9602d6a4d8
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-09-30 18:38:01 +0200

    properly register new crafttype properties

--- src/main/java/de/dertoaster/movecrafttteadditions/init/TTEAdditionsCraftTypeProperties.java
+++ src/main/java/de/dertoaster/movecrafttteadditions/init/TTEAdditionsCraftTypeProperties.java
@@ -1,16 +1,20 @@
 package de.dertoaster.movecrafttteadditions.init;
 
-import de.dertoaster.movecrafttteadditions.TTEAdditionsPlugin;
 import net.countercraft.movecraft.craft.type.CraftType;
+import net.countercraft.movecraft.craft.type.property.BooleanProperty;
 import net.countercraft.movecraft.craft.type.property.IntegerProperty;
 import org.bukkit.NamespacedKey;
 
 public class TTEAdditionsCraftTypeProperties {
 
-    public static final NamespacedKey EXPLOSION_ARMING_DISTANCE = new NamespacedKey(TTEAdditionsPlugin.INSTANCE, "explosion_arming_distance");
+    public static final NamespacedKey EXPLOSION_ARMING_DISTANCE = new NamespacedKey("movecraft-tte-additions", "explosion_arming_distance");
+    public static final NamespacedKey CRUISE_SIGNS_MUST_ALIGN = new NamespacedKey("movecraft-tte-additions", "cruise_signs_must_align");
+    public static final NamespacedKey SKIP_CRUISE_SIGN_VALIDATION_WHEN_ON_CARRIER = new NamespacedKey("movecraft-tte-additions", "cruise_signs_must_align_skip_carrier");
 
     public static void register() {
         CraftType.registerProperty(new IntegerProperty("explosionArmingDistance", EXPLOSION_ARMING_DISTANCE, type -> -1));
+        CraftType.registerProperty(new BooleanProperty("cruiseSignsMustAlign", CRUISE_SIGNS_MUST_ALIGN, type -> false));
+        CraftType.registerProperty(new BooleanProperty("cruiseSignsMustAlignSkipCarrier", SKIP_CRUISE_SIGN_VALIDATION_WHEN_ON_CARRIER, type -> false));
     }
 
 }
