86a0a1ae662b972dceedd13507540e40335196b8
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-01-07 19:02:58 +0100

    adjust to movecraft changes

--- src/main/java/de/dertoaster/movecrafttteadditions/craft/SubcraftRollCraft.java
+++ src/main/java/de/dertoaster/movecrafttteadditions/craft/SubcraftRollCraft.java
@@ -9,23 +9,40 @@ import net.countercraft.movecraft.craft.PilotedCraft;
 import net.countercraft.movecraft.craft.type.CraftType;
 import net.countercraft.movecraft.localisation.I18nSupport;
 import org.bukkit.Axis;
+import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.ref.WeakReference;
+import java.util.UUID;
 
 public class SubcraftRollCraft extends BaseCraft implements PilotedCraft {
 
-    private final Player pilot;
+    private WeakReference<Player> pilot;
+    private final UUID pilotUUID;
 
     public SubcraftRollCraft(@NotNull CraftType type, @NotNull World world, @NotNull Player pilot) {
         super(type, world);
-        this.pilot = pilot;
+        this.pilot = new WeakReference<>(pilot);
+        // Copy UUID just to be safe
+        this.pilotUUID = UUID.fromString(pilot.getUniqueId().toString());
     }
 
-    @NotNull
+    @Nullable
     @Override
     public Player getPilot() {
-        return pilot;
+        if (this.pilot.get() == null) {
+            this.pilot = new WeakReference<>(Bukkit.getPlayer(this.getPilotUUID()));
+            this.setAudience(this.pilot.get());
+        }
+        return this.pilot.get();
+    }
+
+    @Override
+    public @NotNull UUID getPilotUUID() {
+        return this.pilotUUID;
     }
 
     @Override
