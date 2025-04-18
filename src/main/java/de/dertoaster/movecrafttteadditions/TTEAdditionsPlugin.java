99f6525687ecc2932ddb2037e76c2d57a18864ab
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-04-18 22:20:06 +0200

    register command

--- src/main/java/de/dertoaster/movecrafttteadditions/TTEAdditionsPlugin.java
+++ src/main/java/de/dertoaster/movecrafttteadditions/TTEAdditionsPlugin.java
@@ -1,5 +1,6 @@
 package de.dertoaster.movecrafttteadditions;
 
+import de.dertoaster.movecrafttteadditions.command.CraftTypeMergeInfoCommand;
 import de.dertoaster.movecrafttteadditions.commandrestrictor.CommandRestriction;
 import de.dertoaster.movecrafttteadditions.commandrestrictor.listener.CommandListener;
 import de.dertoaster.movecrafttteadditions.commandrestrictor.listener.CraftDetectOrUpdateListener;
@@ -11,10 +12,14 @@ import de.dertoaster.movecrafttteadditions.listener.CraftRotateListener;
 import de.dertoaster.movecrafttteadditions.listener.CraftSinkListener;
 import de.dertoaster.movecrafttteadditions.listener.CraftTranslateListener;
 import de.dertoaster.movecrafttteadditions.sign.*;
+import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import net.countercraft.movecraft.craft.CraftManager;
 import net.countercraft.movecraft.sign.MovecraftSignRegistry;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TTEAdditionsPlugin extends JavaPlugin {
@@ -37,6 +42,13 @@ public final class TTEAdditionsPlugin extends JavaPlugin {
         getConfig();
 
         TTEAdditionsCraftDataTags.register();
+
+        // REgister commands
+        LifecycleEventManager<Plugin> manager = this.getLifecycleManager();
+        manager.registerEventHandler(LifecycleEvents.COMMANDS, event -> {
+            final Commands commands = event.registrar();
+            CraftTypeMergeInfoCommand.register(commands);
+        });
 
         Bukkit.getServer().getPluginManager().registerEvents(new CraftPilotListener(), this);
         Bukkit.getServer().getPluginManager().registerEvents(new CraftTranslateListener(), this);
